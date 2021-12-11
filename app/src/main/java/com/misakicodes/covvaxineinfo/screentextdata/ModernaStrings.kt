f610dfd5ffc4ab4604bc9700b98a0f64dd6642db
misakidebugged <misakidebugged@gmail.com>
2021-12-11 14:57:19 +0200

    General Moderna Information has been Provided

--- app/src/main/java/com/misakicodes/covvaxineinfo/screentextdata/ModernaStrings.kt
+++ app/src/main/java/com/misakicodes/covvaxineinfo/screentextdata/ModernaStrings.kt
@@ -1,27 +1,30 @@
 package com.misakicodes.covvaxineinfo.screentextdata
 
 
-val detailsInFullModerna = "The vaccine arose from a collaboration between Oxford University's Jenner" +
-        " Institute and Vaccitech, a private company spun off from the university, with financing from Oxford " +
-        "Sciences Innovation, Google Ventures, and Sequoia Capital, among others.[74] The first batch " +
-        "of the COVID-19 vaccine produced for clinical testing was developed by Oxford University's " +
-        "Jenner Institute and the Oxford Vaccine Group in collaboration with Italian manufacturer " +
-        "Advent Srl located in Pomezia.[75] The team is led by Sarah Gilbert, Adrian Hill, Andrew " +
-        "Pollard, Teresa Lambe, Sandy Douglas and Catherine Green.[76][75]"
+val detailsInFullModerna = "The Moderna COVID‑19 vaccine, codenamed mRNA-1273 is sold under the brand name \"Spikevax\" \n" +
+        "It is developed by the American company Moderna, United States National Institute of Allergy and Infectious Diseases (NIAID), " +
+        "and the Biomedical Advanced Research and Development Authority (BARDA). \n\n" +
+        "It is an RNA vaccine which is a type of vaccine that uses a copy of a molecule called messenger RNA " +
+        "(mRNA) to produce an immune response."
 
-val prosContentModerna = ""
+val prosContentModerna = "- 94% Effective"
 
-val consContentModerna = ""
+val consContentModerna = "- Rarely, some people have had heart problems after getting the mRNA Vaccine"
 
-val NODNABSModerna = "Doses Needed: One (1)\nBooster Shots Needed: Four (4)"
+val NODNABSModerna = "Doses Needed: TWO (28 Days Apart)\nBooster Shots Needed: One, for those with weak immune systems"
 
-val descMapModerna = "A map detailing where Astrazeneca is Authorized"
+val descMapModerna = "A map detailing where Moderna is Authorized"
 
-val areasVaccineApprovedModerna = "AstraZeneca is Approved in the following regions"
+val areasVaccineApprovedModerna = "Moderna is Approved in the following regions"
 
-val titleToControversiesModerna = "What to know about AstraZeneca and its Side-effects"
+val titleToControversiesModerna = "What to know about Moderna"
 
-val controversiesModerna = "Blah BLah blllllllllllllljjjjj hsssssssssssshhshshsh hshshsh  shshsh" +
-        "shshshhsjkkk sksjksjs ksjkshishs kskshs kkkkks skskshkshs sksk s"
+val controversiesModerna = "A Lot of controversy comes from the fact SpikeVax (Moderna) is an mRNA vaccine, Before 2020 " +
+        "no mRNA technology platform (drug or vaccine) had been authorized for use in humans, " +
+        "so there has always been a risk of unknown effects. 3 years of ongoing tests are still insufficient to " +
+        "conclude them as \"safe\" but due to the emergency of the Covid crisis they had to be approved to meet " +
+        "the required number of people that to be vaccinated for normal life to resume. \n\n" +
+        "Nevertheless, so far its side effects do not differ much to those of traditional vaccines and have " +
+        "proven themselves as very effective vaccines."
 
 val webpageModerna = "https://en.wikipedia.org/wiki/Moderna_COVID-19_vaccine"
