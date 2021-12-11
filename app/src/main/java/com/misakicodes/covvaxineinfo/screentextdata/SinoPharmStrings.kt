39ab5c27ff8d19db9e67a2bf6e2d985ce59bcdeb
misakidebugged <misakidebugged@gmail.com>
2021-12-11 15:17:55 +0200

    General Sinopharm Information has been Provided

--- app/src/main/java/com/misakicodes/covvaxineinfo/screentextdata/SinoPharmStrings.kt
+++ app/src/main/java/com/misakicodes/covvaxineinfo/screentextdata/SinoPharmStrings.kt
@@ -1,27 +1,40 @@
 package com.misakicodes.covvaxineinfo.screentextdata
 
 
-val detailsInFullSinoPharm = "The vaccine arose from a collaboration between Oxford University's Jenner" +
-        " Institute and Vaccitech, a private company spun off from the university, with financing from Oxford " +
-        "Sciences Innovation, Google Ventures, and Sequoia Capital, among others.[74] The first batch " +
-        "of the COVID-19 vaccine produced for clinical testing was developed by Oxford University's " +
-        "Jenner Institute and the Oxford Vaccine Group in collaboration with Italian manufacturer " +
-        "Advent Srl located in Pomezia.[75] The team is led by Sarah Gilbert, Adrian Hill, Andrew " +
-        "Pollard, Teresa Lambe, Sandy Douglas and Catherine Green.[76][75]"
+val detailsInFullSinoPharm = "The Sinopharm Vaccine or BBIBP-CorV has been developed by the " +
+        "Beijing Bio-Institute of Biological Products(BBIBP) in collaboration with the Sinopharm " +
+        "Vaccine or BBIBP-CorV. The vaccine was given authorization by China's National " +
+        "Medical Products Administration on 31st December 2020 and its emergency use " +
+        "against SARS-CoV-2 by the World Health Organization on 7th May 2021\n\n" +
+        "It is an inactivated virus Covid-19 vaccine. It uses a killed or inactivated " +
+        "virus which is made to enter the body by means of a vaccine. " +
+        "It is treated as a foreign antigen by the body’s immune system, which responds by " +
+        "the formation of antibodies, which in turn build up immunity."
 
-val prosContentSinoPharm = ""
+val prosContentSinoPharm = "- 79% Effective"
 
-val consContentSinoPharm = ""
+val consContentSinoPharm = "- 50% effective against variant infection, but 90%+ effective against death"
 
-val NODNABSSinoPharm = "Doses Needed: One (1)\nBooster Shots Needed: Four (4)"
+val NODNABSSinoPharm = "Doses Needed: TWO (3 Weeks Apart)\nBooster Shots Needed: None so far"
 
-val descMapSinoPharm = "A map detailing where Astrazeneca is Authorized"
+val descMapSinoPharm = "A map detailing where BBIBP-CorV is Authorized"
 
-val areasVaccineApprovedSinoPharm = "AstraZeneca is Approved in the following regions"
+val areasVaccineApprovedSinoPharm = "BBIBP-CorV is Approved in the following regions"
 
-val titleToControversiesSinoPharm = "What to know about AstraZeneca and its Side-effects"
+val titleToControversiesSinoPharm = "What to know about Sinopharm's BBIBP-CorV"
 
-val controversiesSinoPharm= "Blah BLah blllllllllllllljjjjj hsssssssssssshhshshsh hshshsh  shshsh" +
-        "shshshhsjkkk sksjksjs ksjkshishs kskshs kkkkks skskshkshs sksk s"
+val controversiesSinoPharm= "Like the other Vaccines, BBIBP-CorV carries the usall side-effects such as \n" +
+        "\n" +
+        "    Tiredness\n" +
+        "    Muscle Pain\n" +
+        "    Fatigue\n" +
+        "    Fever With Chills\n" +
+        "    Nausea Or Vomiting\n" +
+        "    Weakness\n" +
+        "    Headache\n" +
+        "    Acute Disseminated Encephalomyelitis (ADEM)\n\n" +
+        "As it uses an inactivated virus, it is considered safer in the long run, away from the " +
+        "potential mRNA vaccine long term side-effects that are yet unknown. BBIBP-CorV also has a " +
+        "better storage requirement, facilitating its distribution world wide."
 
 val webpageSinoPharm = "https://en.wikipedia.org/wiki/Sinopharm_BIBP_COVID-19_vaccine"
